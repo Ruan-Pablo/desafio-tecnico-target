52ef3b2526bf35c0d4274ec746d3b326f59492de
Ruan-Pablo <ruestacio@gmail.com>
2024-10-03 18:50:44 -0300

    codigos ok

---
+++ 3-trecho-codigo.py
@@ -0,0 +1,11 @@
+# int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+
+INDICE = 12
+soma = 0
+k = 1
+
+while k < INDICE:
+    k +=1
+    soma += k
+
+print(soma)
